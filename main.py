5d3e860c4ff5a3b195d6c9ff6192d066ed57506c
Veronika Bazic <veronikab@mail.com>
2026-01-30 14:40:24 +0100

    Izmena u količini laptopa i uklonjen prikaz proizvoda na main grani.

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from product import Product
 from product_manager import ProductManager
 
 manager = ProductManager() #Kreiranje instance
-p1 = Product("Laptop", 50000, 5) #Kreiranje proizvoda p1
+p1 = Product("Laptop", 50000, 10) #Kreiranje proizvoda p1
 p2 = Product("Miš", 2000, 10) #Kreiranje proizvoda p2
 manager.add_product(p1) #Pozivanje funkcije
 manager.add_product(p2)
@@ -11,5 +11,4 @@ manager.add_product(p2)
 print("\n----TESTIRANJE BRISANJA ----")
 manager.remove_product("Miš") #Pozivanje funkcije
 
-manager.display_all_products() #Pozivanje funkcije
-print(f"Ukupna vrednost: {manager.total_value()} dinara.")
+
